16263d3ffec352ff024859a0a1a0dd8c20e694e4
Aleksey29247 <alik29247@gmail.com>
2022-01-04 16:50:17 +0700

    BMI

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,11 +1,14 @@
 public class BmiService {
 
-    public BmiService() {}
-    public static double calculate(int m, int h)
-    {
-        if ((m<=0)||(h<=0))
+    public BmiService() {
+    }
+
+    public static double calculate(int m, int h) {
+        if ((m <= 0) || (h <= 0)) {
             return -1;
-        return m/Math.pow((double)(h)/100,2);
+        }
+
+        return m / Math.pow((double) (h) / 100, 2);
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,13 @@
 public class Main {
-    public static  void main (String [] args) {
-        int h=172;
-        int m=74;
-        BmiService cBmi=new BmiService();
-        double bmi=cBmi.calculate(m,h);
-        if (bmi>0)
-            System.out.printf (" Индекс массы тела  %.2f кг/м²\n",bmi);
-        else
-            System.out.printf (" Не правельно указаны данные\n");
+    public static void main(String[] args) {
+        int h = 172;
+        int m = 74;
+        BmiService cBmi = new BmiService();
+        double bmi = cBmi.calculate(m, h);
+        if (bmi > 0) {
+            System.out.printf(" Индекс массы тела  %.2f кг/м²\n", bmi);
+        } else {
+            System.out.printf(" Не правельно указаны данные\n");
+        }
     }
 }
